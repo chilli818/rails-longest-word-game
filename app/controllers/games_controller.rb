e72f5fc90beeddb72fee9f2aafbf114cd2fb7677
Charlie Song <csson@student.unimelb.edu.au>
2021-08-24 10:29:29 +1000

    rails longest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,25 @@
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = (0...10).map { (65 + rand(26)).chr }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+end
